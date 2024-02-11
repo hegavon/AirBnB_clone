b739dc4f7da9df2e601dd72d9c02dc1228f8d595
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-02-11 12:21:49 +0100

    Adding the City class

---
+++ models/city.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module City class
+"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """City class that inherits from BaseModel."""
+    state_id = ""
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize City class."""
+        super().__init__(*args, **kwargs)
