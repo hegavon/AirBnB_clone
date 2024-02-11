db13125c696f461bde35d291b821f3a968ab5077
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-02-11 12:29:32 +0100

    Adding the User class

---
+++ models/user.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module User class
+"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """User class that inherits from BaseModel."""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize User class."""
+        super().__init__(*args, **kwargs)
+        self.email = ""
+        self.password = ""
+        self.first_name = ""
+        self.last_name = ""
