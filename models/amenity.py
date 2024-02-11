529420d24cd372dcc06c5b274e03bb766df0fef0
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-02-11 12:25:15 +0100

    Adding the Amenity class

---
+++ models/amenity.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module Amenity class
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Amenity class that inherits from BaseModel."""
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize Amenity class."""
+        super().__init__(*args, **kwargs)
